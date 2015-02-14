5a0001ec01932b16dc43135225ca4d5d7bf6708c
mx.tian <mx.tian@qq.com>
2015-02-14 15:06:22 +0800

    修改版权声明

--- src/main/java/cn/itganhuo/app/common/EncryptablePropertyPlaceholderConfigurer.java
+++ src/main/java/cn/itganhuo/app/common/EncryptablePropertyPlaceholderConfigurer.java
@@ -1,12 +1,18 @@
 /*
- * 1. This project consists of JAVA private school online learning community group Friends co-creator
- *  [QQ group 329232140].
- * 2. See the list of IT dry technology sharing network [http://www.itganhuo.cn/teams].
- * 3. The author does not guarantee the quality of the project and its stability, reliability, and security
- *  does not bear any responsibility.
- * 1、本项目由JAVA私塾在线学习社区群友共同创作[QQ群329232140]；
- * 2、作者名单详见IT干货技术分享网[http://www.itganhuo.cn/teams]；
- * 3、作者不保证本项目质量并对其稳定性、可靠性和安全性不承担任何责任。
+ * Copyright 2014-2024 the https://github.com/xiaoxing598/itganhuo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This project consists of JAVA private school online learning community group Friends co-creator [QQ group 329232140].
+ * 本项目由JAVA私塾在线学习社区群友共同创作[QQ群329232140];
+ * See the list of IT dry technology sharing network [http://www.itganhuo.cn/teams].
+ * 作者名单详见IT干货技术分享网[http://www.itganhuo.cn/teams];
+ * The author does not guarantee the quality of the project and its stability, reliability, and security does not bear any responsibility.
+ * 作者不保证本项目质量并对其稳定性、可靠性和安全性不承担任何责任.
  */
 package cn.itganhuo.app.common;
 
@@ -28,9 +34,11 @@ import cn.itganhuo.app.exception.InternalException;
  * @author 深圳-小兴
  *
  */
-public class EncryptablePropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+public class EncryptablePropertyPlaceholderConfigurer extends
+		PropertyPlaceholderConfigurer {
 
-	private final static Logger log = LogManager.getLogger(EncryptablePropertyPlaceholderConfigurer.class.getName());
+	private final static Logger log = LogManager
+			.getLogger(EncryptablePropertyPlaceholderConfigurer.class.getName());
 
 	private String key = "http://www.itganhuo.cn";
 
@@ -39,23 +47,35 @@ public class EncryptablePropertyPlaceholderConfigurer extends PropertyPlaceholde
 		this.key = key;
 	}
 
-	protected void processProperties(ConfigurableListableBeanFactory beanFactory, Properties props) throws BeansException {
+	protected void processProperties(
+			ConfigurableListableBeanFactory beanFactory, Properties props)
+			throws BeansException {
 		try {
 			String DriverClassName = props.getProperty("jdbc.driverClassName");
 			if (DriverClassName != null) {
-				props.setProperty("jdbc.driverClassName", new String(Aes.decrypt(Aes.parseHexStr2Byte(DriverClassName), key)));
+				props.setProperty(
+						"jdbc.driverClassName",
+						new String(Aes.decrypt(
+								Aes.parseHexStr2Byte(DriverClassName), key)));
 			}
 			String Url = props.getProperty("jdbc.url");
 			if (Url != null) {
-				props.setProperty("jdbc.url", new String(Aes.decrypt(Aes.parseHexStr2Byte(Url), key)));
+				props.setProperty("jdbc.url",
+						new String(Aes.decrypt(Aes.parseHexStr2Byte(Url), key)));
 			}
 			String Username = props.getProperty("jdbc.username");
 			if (Username != null) {
-				props.setProperty("jdbc.username", new String(Aes.decrypt(Aes.parseHexStr2Byte(Username), key)));
+				props.setProperty(
+						"jdbc.username",
+						new String(Aes.decrypt(Aes.parseHexStr2Byte(Username),
+								key)));
 			}
 			String Password = props.getProperty("jdbc.password");
 			if (Password != null) {
-				props.setProperty("jdbc.password", new String(Aes.decrypt(Aes.parseHexStr2Byte(Password), key)));
+				props.setProperty(
+						"jdbc.password",
+						new String(Aes.decrypt(Aes.parseHexStr2Byte(Password),
+								key)));
 			}
 
 			super.processProperties(beanFactory, props);
--- src/main/java/cn/itganhuo/app/common/pool/ConstantPool.java
+++ src/main/java/cn/itganhuo/app/common/pool/ConstantPool.java
@@ -1,12 +1,18 @@
 /*
- * 1. This project consists of JAVA private school online learning community group Friends co-creator
- *  [QQ group 329232140].
- * 2. See the list of IT dry technology sharing network [http://www.itganhuo.cn/teams].
- * 3. The author does not guarantee the quality of the project and its stability, reliability, and security
- *  does not bear any responsibility.
- * 1、本项目由JAVA私塾在线学习社区群友共同创作[QQ群329232140]；
- * 2、作者名单详见IT干货技术分享网[http://www.itganhuo.cn/teams]；
- * 3、作者不保证本项目质量并对其稳定性、可靠性和安全性不承担任何责任。
+ * Copyright 2014-2024 the https://github.com/xiaoxing598/itganhuo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This project consists of JAVA private school online learning community group Friends co-creator [QQ group 329232140].
+ * 本项目由JAVA私塾在线学习社区群友共同创作[QQ群329232140];
+ * See the list of IT dry technology sharing network [http://www.itganhuo.cn/teams].
+ * 作者名单详见IT干货技术分享网[http://www.itganhuo.cn/teams];
+ * The author does not guarantee the quality of the project and its stability, reliability, and security does not bear any responsibility.
+ * 作者不保证本项目质量并对其稳定性、可靠性和安全性不承担任何责任.
  */
 package cn.itganhuo.app.common.pool;
 
