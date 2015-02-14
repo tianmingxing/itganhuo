2f3d4fc1b62f6c6aef10a7986b68d4734eb0aae8
mx.tian <mx.tian@qq.com>
2015-02-14 17:10:04 +0800

    更新公共数据库访问地址
    Signed-off-by: mx.tian <mx.tian@qq.com>

--- src/test/java/cn/itganhuo/app/common/TestAes.java
+++ src/test/java/cn/itganhuo/app/common/TestAes.java
@@ -10,5 +10,9 @@ public class TestAes {
 	public void testEncrypt() {
 		System.out.println(Aes.parseByte2HexStr(Aes.encrypt("jdbc:mysql://localhost:3306/itganhuo?useUnicode=true&characterEncoding=utf-8", key)));
 		System.out.println(Aes.parseByte2HexStr(Aes.encrypt("root", key)));
+		
+		//生成公共测试库连接信息
+		System.out.println(Aes.parseByte2HexStr(Aes.encrypt("jdbc:mysql://114.215.171.211:3306/test?useUnicode=true&characterEncoding=utf-8", key)));
+		System.out.println(Aes.parseByte2HexStr(Aes.encrypt("test", key)));
 	}
 }
